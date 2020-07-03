801e2daffbabb1b71039ff46965f10f8def6a356
talalzeini <56060993+talalzeini@users.noreply.github.com>
2020-07-03 00:48:40 -0700

    added single page for each product using js

--- script.js
+++ script.js
@@ -43,7 +43,14 @@ function whiteToBlack(){
 function goToProduct(){
     document.getElementById("single-product").classList.remove("hide");
     document.getElementById("not-single-product").classList.add("hide")
-
+    document.getElementById("homeButton").classList.remove("hide")
+}
+function goToHome(){
+    console.log("working")
+    document.getElementById("single-product").classList.add("hide");
+    document.getElementById("not-single-product").classList.remove("hide")
+    document.getElementById("homeButton").classList.add("hide")
+    
 }
 
 
@@ -55,14 +62,14 @@ function iPhone11(){
     
     document.getElementById("lastStar").classList.add("fa-star");
 
-    document.getElementById("condition").innerText += "NEW"
+    document.getElementById("condition").innerText = "NEW"
 
-    document.getElementById("productName").innerText += "Iphone 11"
+    document.getElementById("productName").innerText = "Iphone 11"
 
-    document.getElementById("productCompany").innerText += " Apple"
+    document.getElementById("productCompany").innerText = " Apple"
 
-    document.getElementById("productCode").innerText += "Product Code: IRS74786"
-    document.getElementById("productPrice").innerText += "USD $699.00"
+    document.getElementById("productCode").innerText = "Product Code: IRS74786"
+    document.getElementById("productPrice").innerText = "USD $699.00"
 }
 function yeezyBoost(){
     var yeezyBoost = document.getElementById("yeezyBoost").src;
@@ -70,14 +77,14 @@ function yeezyBoost(){
     
     document.getElementById("lastStar").classList.add("fa-star");
 
-    document.getElementById("condition").innerText += "NEW"
+    document.getElementById("condition").innerText = "NEW"
 
-    document.getElementById("productName").innerText += "Adidas Yeezy Boost"
+    document.getElementById("productName").innerText = "Adidas Yeezy Boost"
 
-    document.getElementById("productCompany").innerText += " Adidas"
+    document.getElementById("productCompany").innerText = " Adidas"
 
-    document.getElementById("productCode").innerText += "Product Code: IRS36782"
-    document.getElementById("productPrice").innerText += "USD $369.00"
+    document.getElementById("productCode").innerText = "Product Code: IRS36782"
+    document.getElementById("productPrice").innerText = "USD $369.00"
 }
 
 function supremeSweatShirt(){
@@ -86,14 +93,14 @@ function supremeSweatShirt(){
     
     document.getElementById("lastStar").classList.add("fa-star");
 
-    document.getElementById("condition").innerText += "NEW"
+    document.getElementById("condition").innerText = "NEW"
 
-    document.getElementById("productName").innerText += "Supreme Sweatshirt"
+    document.getElementById("productName").innerText = "Supreme Sweatshirt"
 
-    document.getElementById("productCompany").innerText += " Supreme"
+    document.getElementById("productCompany").innerText = " Supreme"
 
-    document.getElementById("productCode").innerText += "Product Code: IRS52763"
-    document.getElementById("productPrice").innerText += "USD $199.00"
+    document.getElementById("productCode").innerText = "Product Code: IRS52763"
+    document.getElementById("productPrice").innerText = "USD $199.00"
 }
 
 function blackWatch(){
@@ -101,14 +108,14 @@ function blackWatch(){
     document.getElementById("mainProduct").src = blackWatchURL
     document.getElementById("lastStar").classList.add("fa-star-o");
 
-    document.getElementById("condition").innerText += "NEW"
+    document.getElementById("condition").innerText = "NEW"
 
-    document.getElementById("productName").innerText += "Black Watch"
+    document.getElementById("productName").innerText = "Black Watch"
 
-    document.getElementById("productCompany").innerText += " Curdden"
+    document.getElementById("productCompany").innerText = " Curdden"
 
-    document.getElementById("productCode").innerText += "Product Code: IRS23452"
-    document.getElementById("productPrice").innerText += "USD $32.00"
+    document.getElementById("productCode").innerText = "Product Code: IRS23452"
+    document.getElementById("productPrice").innerText = "USD $32.00"
 
 }
 function HollisterShirt(){
@@ -116,42 +123,196 @@ function HollisterShirt(){
     document.getElementById("mainProduct").src = HollisterShirtURL
     document.getElementById("lastStar").classList.add("fa-star-o");
 
-    document.getElementById("condition").innerText += "NEW"
+    document.getElementById("condition").innerText = "NEW"
 
-    document.getElementById("productName").innerText += "Hollister Shirt"
+    document.getElementById("productName").innerText = "Hollister Shirt"
 
-    document.getElementById("productCompany").innerText += " Hollister"
+    document.getElementById("productCompany").innerText = " Hollister"
 
-    document.getElementById("productCode").innerText += "Product Code: IRS25627"
-    document.getElementById("productPrice").innerText += "USD $23.00"
+    document.getElementById("productCode").innerText = "Product Code: IRS25627"
+    document.getElementById("productPrice").innerText = "USD $23.00"
 }
 function YeezyGreen(){
     var YeezyGreenURL = document.getElementById("YeezyGreen").src;
     document.getElementById("mainProduct").src = YeezyGreenURL
     document.getElementById("lastStar").classList.add("fa-star");
 
-    document.getElementById("condition").innerText += "NEW"
-    document.getElementById("productName").innerText += "Adidas Yeezy Boost"
+    document.getElementById("condition").innerText = "NEW"
+    document.getElementById("productName").innerText = "Adidas Yeezy Boost"
 
-    document.getElementById("productCompany").innerText += " Adidas"
+    document.getElementById("productCompany").innerText = " Adidas"
 
-    document.getElementById("productCode").innerText += "Product Code: IRS36781"
-    document.getElementById("productPrice").innerText += "USD $319.00"
+    document.getElementById("productCode").innerText = "Product Code: IRS36781"
+    document.getElementById("productPrice").innerText = "USD $319.00"
 }
 function LeatherWatch(){
     var LeatherWatchURL = document.getElementById("LeatherWatch").src;
     document.getElementById("mainProduct").src = LeatherWatchURL
     document.getElementById("lastStar").classList.add("fa-star-o");
 
-    document.getElementById("condition").innerText += "NEW"
-    document.getElementById("productCompany").innerText += " Curdden"
-    document.getElementById("productName").innerText += "Leather Watch"
-    document.getElementById("productCode").innerText += "Product Code: IRS23453"
-    document.getElementById("productPrice").innerText += "USD $23.00"
+    document.getElementById("productName").innerText = "NEW"
+    document.getElementById("productCompany").innerText = " Curdden"
+    document.getElementById("productName").innerText = "Leather Watch"
+    document.getElementById("productCode").innerText = "Product Code: IRS23453"
+    document.getElementById("productPrice").innerText = "USD $23.00"
     
 }
 
+   
+function airPods(){
+    var airPods = document.getElementById("airPods").src;
+    document.getElementById("mainProduct").src = airPods
+    document.getElementById("lastStar").classList.add("fa-star");
 
+    document.getElementById("condition").innerText = "NEW"
+    document.getElementById("productCompany").innerText = " Apple"
+    document.getElementById("productName").innerText = "Airpods pro"
+    document.getElementById("productCode").innerText = "Product Code: IRS74796"
+    document.getElementById("productPrice").innerText = "USD $249.00"
+}
+function airForceSE(){
+    var airForceSE = document.getElementById("airForceSE").src;
+    document.getElementById("mainProduct").src = airForceSE
+    document.getElementById("lastStar").classList.add("fa-star");
+
+    document.getElementById("condition").innerText = "NEW"
+    document.getElementById("productCompany").innerText = " Nike"
+    document.getElementById("productName").innerText = "Nike Air Force SE"
+    document.getElementById("productCode").innerText = "Product Code: IRS22344"
+    document.getElementById("productPrice").innerText = "USD $157.00"
+}
+function AFShirt(){
+    var AFShirt = document.getElementById("AFShirt").src;
+    document.getElementById("mainProduct").src = AFShirt
+    document.getElementById("lastStar").classList.add("fa-star-o");
+
+    document.getElementById("condition").innerText = "NEW"
+    document.getElementById("productCompany").innerText = " Abercrombie & Fitch"
+    document.getElementById("productName").innerText = "A&F Shirt for Men"
+    document.getElementById("productCode").innerText = "Product Code: IRS94399"
+    document.getElementById("productPrice").innerText = "USD $66.00"
+}
+function googleHome(){
+    var googleHome = document.getElementById("googleHome").src;
+    document.getElementById("mainProduct").src = googleHome
+    document.getElementById("lastStar").classList.add("fa-star");
+
+    document.getElementById("condition").innerText = "NEW"
+    document.getElementById("productCompany").innerText = " Google"
+    document.getElementById("productName").innerText = "Google Home Speaker"
+    document.getElementById("productCode").innerText = "Product Code: IRS77653"
+    document.getElementById("productPrice").innerText = "USD $48.99"
+}
+function fifa20(){
+    var fifa20 = document.getElementById("fifa20").src;
+    document.getElementById("mainProduct").src = fifa20
+    document.getElementById("lastStar").classList.add("fa-star");
+
+    document.getElementById("condition").innerText = "NEW"
+    document.getElementById("productCompany").innerText = " Sony"
+    document.getElementById("productName").innerText = "FIFA 20"
+    document.getElementById("productCode").innerText = "Product Code: IRS14679"
+    document.getElementById("productPrice").innerText = "USD $59.99"
+}
+function got(){
+    var got = document.getElementById("got").src;
+    document.getElementById("mainProduct").src = got
+    document.getElementById("lastStar").classList.add("fa-star");
+
+    document.getElementById("condition").innerText = "NEW"
+    document.getElementById("productCompany").innerText = " New York Times"
+    document.getElementById("productName").innerText = "Game of Thrones Books"
+    document.getElementById("productCode").innerText = "Product Code: IRS24291"
+    document.getElementById("productPrice").innerText = "USD $156.99"
+}
+function rachelGreen(){
+    var rachelGreen = document.getElementById("rachelGreen").src;
+    document.getElementById("mainProduct").src = rachelGreen
+    document.getElementById("lastStar").classList.add("fa-star-o");
+
+    document.getElementById("condition").innerText = "NEW"
+    document.getElementById("productCompany").innerText = " Pop! Funko"
+    document.getElementById("productName").innerText = "Pop! Rachel Green"
+    document.getElementById("productCode").innerText = "Product Code: IRS39979"
+    document.getElementById("productPrice").innerText = "USD $25.99"
+}
+function digital(){
+    var digital = document.getElementById("digital").src;
+    document.getElementById("mainProduct").src = digital
+    document.getElementById("lastStar").classList.add("fa-star");
+
+    document.getElementById("condition").innerText = "NEW"
+    document.getElementById("productCompany").innerText = " SKMEI"
+    document.getElementById("productName").innerText = "Digital Watch"
+    document.getElementById("productCode").innerText = "Product Code: IRS13348"
+    document.getElementById("productPrice").innerText = "USD $18.99"
+}
+function nikeBlue(){
+    var nikeBlue = document.getElementById("nikeBlue").src;
+    document.getElementById("mainProduct").src = nikeBlue
+    document.getElementById("lastStar").classList.add("fa-star");
+    document.getElementById("condition").innerText = "USED"
+    document.getElementById("productCompany").innerText = " Nike"
+    document.getElementById("productName").innerText = "Nike Air Force Blue"
+    document.getElementById("productCode").innerText = "Product Code: IRS22346"
+    document.getElementById("productPrice").innerText = "USD $119.99"
+    document.getElementById("condition").style.backgroundColor = "red";
+}
+function ipad(){
+    var ipad = document.getElementById("ipad").src;
+    document.getElementById("mainProduct").src = ipad
+    document.getElementById("lastStar").classList.add("fa-star");
+    document.getElementById("condition").innerText = "USED"
+    document.getElementById("productCompany").innerText = " Apple"
+    document.getElementById("productName").innerText = "11-inch iPad Pro"
+    document.getElementById("productCode").innerText = "Product Code: IRS74646"
+    document.getElementById("productPrice").innerText = "USD $599.99"
+    document.getElementById("condition").style.backgroundColor = "red";
+}
+function dany(){
+    var dany = document.getElementById("dany").src;
+    document.getElementById("mainProduct").src = dany
+    document.getElementById("lastStar").classList.add("fa-star-o");
+    document.getElementById("condition").innerText = "USED"
+    document.getElementById("productCompany").innerText = " Pop! Funko"
+    document.getElementById("productName").innerText = "Pop! Daenerys Targaryen"
+    document.getElementById("productCode").innerText = "Product Code: IRS39983"
+    document.getElementById("productPrice").innerText = "USD $18.99"
+    document.getElementById("condition").style.backgroundColor = "red";
+}
+function xbox(){
+    var xbox = document.getElementById("xbox").src;
+    document.getElementById("mainProduct").src = xbox
+    document.getElementById("lastStar").classList.add("fa-star");
+    document.getElementById("condition").innerText = "USED"
+    document.getElementById("productCompany").innerText = " Xbox"
+    document.getElementById("productName").innerText = "Xbox Controller "
+    document.getElementById("productCode").innerText = "Product Code: IRS41153"
+    document.getElementById("condition").style.backgroundColor = "red";
+    document.getElementById("productPrice").innerText = "USD $28.00"
+}
+function fortnite(){
+    var fortnite = document.getElementById("fortnite").src;
+    document.getElementById("mainProduct").src = fortnite
+    document.getElementById("lastStar").classList.add("fa-star");
+    document.getElementById("condition").innerText = "USED"
+    document.getElementById("productCompany").innerText = " Sony"
+    document.getElementById("productName").innerText = "PS4 Fortnite "
+    document.getElementById("productCode").innerText = "Product Code: IRS34788"
+    document.getElementById("condition").style.backgroundColor = "red";
+    document.getElementById("productPrice").innerText = "USD $59.99"
+}
+function boss(){
+    var boss = document.getElementById("boss").src;
+    document.getElementById("mainProduct").src = boss
+    document.getElementById("lastStar").classList.add("fa-star-o");
+    document.getElementById("condition").innerText = "USED"
+    document.getElementById("productCompany").innerText = " Hugo Boss"
+    document.getElementById("productName").innerText = "Hugo Boss Leather Watch "
+    document.getElementById("productCode").innerText = "Product Code: IRS56123"
+    document.getElementById("condition").style.backgroundColor = "red";
+    document.getElementById("productPrice").innerText = "USD $88.99"
+}
 
 
 
