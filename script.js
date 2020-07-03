f36bc32c855d14ba772562456acff4accbf47682
talalzeini <56060993+talalzeini@users.noreply.github.com>
2020-07-02 23:50:40 -0700

    deleted product.html and worked on best products

--- script.js
+++ script.js
@@ -40,7 +40,116 @@ function whiteToBlack(){
     document.getElementById("black").classList.remove("hide");
     setTimeout(function() { blackToGreen(); }, 3000);
 }
+function goToProduct(){
+    document.getElementById("single-product").classList.remove("hide");
+    document.getElementById("not-single-product").classList.add("hide")
 
+}
+
+
+
+
+function iPhone11(){
+    var iPhone11URL = document.getElementById("black").src;
+    document.getElementById("mainProduct").src = iPhone11URL
+    
+    document.getElementById("lastStar").classList.add("fa-star");
+
+    document.getElementById("condition").innerText += "NEW"
+
+    document.getElementById("productName").innerText += "Iphone 11"
+
+    document.getElementById("productCompany").innerText += " Apple"
+
+    document.getElementById("productCode").innerText += "Product Code: IRS74786"
+    document.getElementById("productPrice").innerText += "USD $699.00"
+}
+function yeezyBoost(){
+    var yeezyBoost = document.getElementById("yeezyBoost").src;
+    document.getElementById("mainProduct").src = yeezyBoost
+    
+    document.getElementById("lastStar").classList.add("fa-star");
+
+    document.getElementById("condition").innerText += "NEW"
+
+    document.getElementById("productName").innerText += "Adidas Yeezy Boost"
+
+    document.getElementById("productCompany").innerText += " Adidas"
+
+    document.getElementById("productCode").innerText += "Product Code: IRS36782"
+    document.getElementById("productPrice").innerText += "USD $369.00"
+}
+
+function supremeSweatShirt(){
+    var supremeSweatShirt = document.getElementById("supremeSweatShirt").src;
+    document.getElementById("mainProduct").src = supremeSweatShirt
+    
+    document.getElementById("lastStar").classList.add("fa-star");
+
+    document.getElementById("condition").innerText += "NEW"
+
+    document.getElementById("productName").innerText += "Supreme Sweatshirt"
+
+    document.getElementById("productCompany").innerText += " Supreme"
+
+    document.getElementById("productCode").innerText += "Product Code: IRS52763"
+    document.getElementById("productPrice").innerText += "USD $199.00"
+}
+
+function blackWatch(){
+    var blackWatchURL = document.getElementById("blackWatch").src;
+    document.getElementById("mainProduct").src = blackWatchURL
+    document.getElementById("lastStar").classList.add("fa-star-o");
+
+    document.getElementById("condition").innerText += "NEW"
+
+    document.getElementById("productName").innerText += "Black Watch"
+
+    document.getElementById("productCompany").innerText += " Curdden"
+
+    document.getElementById("productCode").innerText += "Product Code: IRS23452"
+    document.getElementById("productPrice").innerText += "USD $32.00"
+
+}
+function HollisterShirt(){
+    var HollisterShirtURL = document.getElementById("HollisterShirt").src;
+    document.getElementById("mainProduct").src = HollisterShirtURL
+    document.getElementById("lastStar").classList.add("fa-star-o");
+
+    document.getElementById("condition").innerText += "NEW"
+
+    document.getElementById("productName").innerText += "Hollister Shirt"
+
+    document.getElementById("productCompany").innerText += " Hollister"
+
+    document.getElementById("productCode").innerText += "Product Code: IRS25627"
+    document.getElementById("productPrice").innerText += "USD $23.00"
+}
+function YeezyGreen(){
+    var YeezyGreenURL = document.getElementById("YeezyGreen").src;
+    document.getElementById("mainProduct").src = YeezyGreenURL
+    document.getElementById("lastStar").classList.add("fa-star");
+
+    document.getElementById("condition").innerText += "NEW"
+    document.getElementById("productName").innerText += "Adidas Yeezy Boost"
+
+    document.getElementById("productCompany").innerText += " Adidas"
+
+    document.getElementById("productCode").innerText += "Product Code: IRS36781"
+    document.getElementById("productPrice").innerText += "USD $319.00"
+}
+function LeatherWatch(){
+    var LeatherWatchURL = document.getElementById("LeatherWatch").src;
+    document.getElementById("mainProduct").src = LeatherWatchURL
+    document.getElementById("lastStar").classList.add("fa-star-o");
+
+    document.getElementById("condition").innerText += "NEW"
+    document.getElementById("productCompany").innerText += " Curdden"
+    document.getElementById("productName").innerText += "Leather Watch"
+    document.getElementById("productCode").innerText += "Product Code: IRS23453"
+    document.getElementById("productPrice").innerText += "USD $23.00"
+    
+}
 
 
 
